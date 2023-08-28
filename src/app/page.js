14f05edee6e02a84de7ebf73eb781fca7010e9a4
Manuela De Luca <106416824+manudl97@users.noreply.github.com>
2023-08-28 10:36:22 -0300

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+"use client"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
